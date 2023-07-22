2ecb5c3c305ff8f641e71d9c864c3b5d9a4b9fc4
h. mikail <>
2023-07-22 03:54:41 -0700

    lsrgest of two num exr submit

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+const x=9
+const y=8
+if(x<=9){
+    console.log(" 9 this is the greates number")
+}
+else {
+    console.log("out numbered or it small than 9")
+}
